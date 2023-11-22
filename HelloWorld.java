db0524e9aad69b36ccd9e66271401c1312a8f7bf
Kiran Kumatkar <139500973+kirankumatkar@users.noreply.github.com>
2023-11-22 17:06:26 +0530

    modified file 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("My name is Kiran..."); 
+        System.out.println("Here is java code to test jenkins");
     }
 }
